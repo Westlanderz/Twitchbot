3e95cc947d902c563cc43a6813ba8ad79cc8f4d2
Westlanderz <noah28129912@gmail.com>
2021-03-29 20:43:17 +0200

    Added header for the addcommand command

---
+++ includes/commands/addcommand.hpp
@@ -0,0 +1,21 @@
+#ifndef addcommand_h
+#define addcommand_h
+
+#include "../command.hpp"
+
+class Addcommand : public Command {
+    public:
+        Addcommand(Bot *);
+
+        void execute(std::string, std::string, bool, bool, std::string) override;
+        bool has_perms_to_run(bool, bool, std::string) override;
+        bool find_name(std::string) override;
+        std::string list_command() override;
+        std::string generate_help_message(const std::string &) override;
+
+    private:
+        std::vector<std::string> names;
+        Bot *bot;
+};
+
+#endif
